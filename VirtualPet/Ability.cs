fc497dad441ed1c045dd82adc7d3922d5ca47929
sabrinachaves <sabrinach08@gmail.com>
2022-08-26 22:47:50 -0300

    Inserção dos menus e características dos pokemons

--- VirtualPet/Ability.cs
+++ VirtualPet/Ability.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using VirtualPet;
 
 namespace VirtualPet
 {
-    public class Pokemon
+    public class Ability
     {
         public string name { get; set; }
-        public int height { get; set; }
-        public int weight { get; set; }
-        public List<Ability> abilities { get; set; }
+        public bool is_hidden  { get; set; }
 
     }
 }
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -3,48 +3,101 @@ using System.Text;
 using System.Text.Json;
 using VirtualPet;
 
-
-Console.WriteLine("1 - Bulbasaur\n" +
-    "2 - Ivysaur\n" +
-    "3 - Venusaur\n" +
-    "4 - Goldeen\n" +
-    "5 - Seadra\n");
-
-int option = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Seja bem-vindo ao Virtual Pet!\n");
 string chosenPokemon = "";
+Menu();
 
-if (option == 1)
-    chosenPokemon = "bulbasaur";
-else if (option == 2)
-    chosenPokemon = "ivysaur";
-else if (option == 3)
-    chosenPokemon = "venusaur";
-else if (option == 4)
-    chosenPokemon = "goldeen";
-else if (option == 5)
-    chosenPokemon = "seadra";
-else if (option == 6)
-    chosenPokemon = "erro";
-
-Console.WriteLine($"O pokemon escolhido foi {chosenPokemon}");
-
-var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{chosenPokemon}");
-var request = new RestRequest("", Method.Get);
-
-var response = client.Execute(request);
-
-if (response.StatusCode == System.Net.HttpStatusCode.OK)
+void Menu()
 {
-    Pokemon pokemon = JsonSerializer.Deserialize<Pokemon>(response.Content);
-    Console.WriteLine(pokemon.url);
+    Console.WriteLine("Os pokemons disponíves são:\n");
+    Console.WriteLine("1 - Bulbasaur\n" +
+        "2 - Ivysaur\n" +
+        "3 - Venusaur\n" +
+        "4 - Goldeen\n" +
+        "5 - Seadra\n");
+
+    Console.WriteLine("Escolha um pokemon através do respectivo número: ");
+    int option = Int32.Parse(Console.ReadLine());
+
+
+    switch (option)
+    {
+        case 1:
+            chosenPokemon = "bulbasaur";
+            feature();
+            break;
+        case 2:
+            chosenPokemon = "ivysaur";
+            feature();
+            break;
+        case 3:
+            chosenPokemon = "venusaur";
+            feature();
+            break;
+        case 4:
+            chosenPokemon = "goldeen";
+            feature();
+            break;
+        case 5:
+            chosenPokemon = "seadra";
+            feature();
+            break;
+        default:
+            Console.WriteLine("Você escolheu uma opção inválida. Escolha novamente!");
+            Menu();
+            break;
+    }
+
+    void feature()
+    {
+        var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{chosenPokemon}");
+        var request = new RestRequest("", Method.Get);
+
+        var response = client.Execute(request);
+
+        Pokemon pokemon = JsonSerializer.Deserialize<Pokemon>(response.Content);
+        if (response.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            Console.WriteLine($"Nome do pokemon: {pokemon.name}");
+            Console.WriteLine($"Altura: {pokemon.height}");
+            Console.WriteLine($"Peso: {pokemon.weight}");
+            AuxMenu();
+        }
+        else
+        {
+            Console.WriteLine(response.ErrorMessage);
+        }
+    }
 }
-    // Console.WriteLine(response.Content);
-    
-else
-    Console.WriteLine(response.ErrorMessage);
 
-//Pokemon pokemon = JsonConvert.DeserializeObject<Pokemon>(response.Content); 
+void AuxMenu()
+{
+    int option2 = 0;
 
+    while (option2 != 1 && option2 != 2)
+    {
+        Console.WriteLine("\nO que deseja fazer agora?" +
+        "\n1 - Adotar este pokemon;" +
+        "\n2 - Voltar ao menu inicial.");
+
+        option2 = Int32.Parse(Console.ReadLine());
+    }
+
+
+
+    switch (option2)
+    {
+        case 1:
+            Console.WriteLine($"\nParabéns! Você adotou o pokemon!");
+            break;
+        case 2:
+            Menu();
+            break;
+        default:
+            Console.WriteLine("Escolha uma opção válida!");
+            break;
+    }
+}
 
 
 Console.ReadKey();
