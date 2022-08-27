08f1c096368452f8bea2fca5c37d40f319770711
sabrinachaves <sabrinach08@gmail.com>
2022-08-26 21:30:55 -0300

    Requisição http da API

---
+++ VirtualPet/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    public class Pokemon
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+}
--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RestSharp;
+using System.Text;
+using System.Text.Json;
+using VirtualPet;
+
+
+Console.WriteLine("1 - Bulbasaur\n" +
+    "2 - Ivysaur\n" +
+    "3 - Venusaur\n" +
+    "4 - Goldeen\n" +
+    "5 - Seadra\n");
+
+int option = Int32.Parse(Console.ReadLine());
+string chosenPokemon = "";
+
+if (option == 1)
+    chosenPokemon = "bulbasaur";
+else if (option == 2)
+    chosenPokemon = "ivysaur";
+else if (option == 3)
+    chosenPokemon = "venusaur";
+else if (option == 4)
+    chosenPokemon = "goldeen";
+else if (option == 5)
+    chosenPokemon = "seadra";
+else if (option == 6)
+    chosenPokemon = "erro";
+
+Console.WriteLine($"O pokemon escolhido foi {chosenPokemon}");
+
+var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{chosenPokemon}");
+var request = new RestRequest("", Method.Get);
+
+var response = client.Execute(request);
+
+if (response.StatusCode == System.Net.HttpStatusCode.OK)
+{
+    Pokemon pokemon = JsonSerializer.Deserialize<Pokemon>(response.Content);
+    Console.WriteLine(pokemon.url);
+}
+    // Console.WriteLine(response.Content);
+    
+else
+    Console.WriteLine(response.ErrorMessage);
+
+//Pokemon pokemon = JsonConvert.DeserializeObject<Pokemon>(response.Content); 
+
+
+
+Console.ReadKey();
